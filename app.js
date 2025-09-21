64338a25eaf9c7a55ea889c127622e9374733d2b
carlos <carlosdanielap89@gmail.com>
2025-09-20 22:29:06 -0600

    leemos la caja de texto y la mostramos en consola

---
+++ app.js
@@ -0,0 +1,11 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos =[];
+
+function agregarAmigo(){
+        //obtenemos el nombre del amigo del cambo de texto
+        const nombreInput = document.getElementById('amigo');
+        //tomamos el valor de este
+        const nombreAmigo=nombreInput.value;
+
+        console.log(nombreAmigo);
+    }
